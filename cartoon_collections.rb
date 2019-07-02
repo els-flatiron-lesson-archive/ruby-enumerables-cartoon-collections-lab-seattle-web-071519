98fbc4e4d3db03f0eb599d75fddb416ad76bb456
Helen Marshall <marshallhelenm@gmail.com>
2019-07-02 22:14:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"
+  cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {
     |item| array.include? cheese_types[item]
   }
