d1d684025808fc8f03fa235fec48515e6f58be71
Helen Marshall <marshallhelenm@gmail.com>
2019-07-02 22:18:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {
-    |item| array.include?(cheese_types[item])
+    |item| cheese_types.include?(item)
   }
 end
